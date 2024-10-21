5bcefbbe707c5c8976aa05beeba9739d1012cf3e
Kapex-Coder <kapex.coder@gmail.com>
2024-10-22 01:54:30 +0530

    Updated: BuilderContent component

--- components/Builder/Content/index.vue
+++ components/Builder/Content/index.vue
@@ -1,16 +1,24 @@
 <script setup lang="ts">
-import { Content, fetchOneEntry, isPreviewing } from '@builder.io/sdk-vue';
+import { Content, fetchOneEntry, isPreviewing } from "@builder.io/sdk-vue";
+
+type Props = {
+  model?: string;
+  userAttributes?: Record<string, any>;
+};
 
 const route = useRoute();
 const runtimeConfig = useRuntimeConfig();
 
 const apiKey = runtimeConfig.public.builderPublicApiKey;
 const canShowContent = ref(false);
-const model = 'page';
 
-const { data: content } = await useAsyncData('builderData', () =>
+const props = withDefaults(defineProps<Props>(), {
+  model: "page",
+});
+
+const { data: content } = await useAsyncData("builderData", () =>
   fetchOneEntry({
-    model,
+    model: props.model,
     apiKey,
     userAttributes: {
       urlPath: route.path,
@@ -25,5 +33,5 @@ canShowContent.value = content.value ? true : isPreviewing(route.path);
   <div v-if="canShowContent">
     <Content :api-key="apiKey" :model="model" :content="content" />
   </div>
-  <div v-else>Content not Found{{apiKey}}</div>
+  <div v-else>Content not Found</div>
 </template>
